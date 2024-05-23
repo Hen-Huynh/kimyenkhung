666004c104ff1ca59bc3958096d027a2015a4899
Hen Huynh <139737865+Hen-Huynh@users.noreply.github.com>
2024-05-23 23:12:10 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('play-button').addEventListener('click', function() {
+    const message = document.getElementById('message');
+    message.textContent = 'Happy Birthday Phượng Hoàng lửa';
+    message.style.display = 'block';
+});
